fc0d0ad2404620deaa316a3bf9a4c522717c17f6
Mckennie <k.koizumi50623@gmail.com>
2025-06-21 10:44:42 +0900

    Revert R back to T

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/via/keymap.c
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_F10   , KC_F11     , KC_F12   , KC_BSPC   , KC_ENT   , KC_LCTL  ,      TO(2)  , TO(1)  , _______  , _______  , _______  , RSFT_T(KC_ESC)
   ),
   [1] = LAYOUT_universal(
-    KC_Q     , KC_W     , KC_E     , KC_R     , KC_R     ,                            KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     ,
+    KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                            KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     ,
     KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                            KC_H     , KC_J     , RCTL_T(KC_K) , RWIN_T(KC_L)    , KC_MINS  ,
     KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                            KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  ,
     LCTL_T(KC_TAB), KC_LGUI, KC_LALT,KC_BSPC  , KC_ENT   ,KC_SPC,   LT(2,KC_LNG1)   , RSFT_T(KC_LNG2), _______, _______,_______, RSFT_T(KC_ESC)
